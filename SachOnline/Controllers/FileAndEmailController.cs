bdfe515f7a0e527028f4f4f5d332bb011c4c0f00
btrongtin891 <btrongtin891@gmail.com>
2021-06-14 07:46:46 +0700

    Update: send email, cart, comfirm order

---
+++ SachOnline/Controllers/FileAndEmailController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using SachOnline.Models;
+using System.Net.Mail;
+using System.Net;
+using System.Net.Mime;
+
+namespace SachOnline.Controllers
+{
+    public class FileAndEmailController : Controller
+    {
+        // GET: FileAndEmail
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SendMail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendMail(Mail model)
+        {
+            //cấu hình tt gmail [khai báo thư viện system.net]
+            var mail = new SmtpClient("smtp.gmail.com", 587)
+            {
+                //khai báo thư viện system.net
+                Credentials = new NetworkCredential(model.From, "sickmyduch4password"),
+                //your email (abc@gmail.com) anhd your password (*****)
+                EnableSsl = true
+            };
+            //tạo email
+            var message = new MailMessage();
+            message.From = new MailAddress(model.From);
+            message.ReplyToList.Add(model.From);
+            message.To.Add(new MailAddress(model.To));
+            message.Subject = model.Subject;
+            message.Body = model.Notes;
+
+            var f = Request.Files["attachment"];
+            var path = Path.Combine(Server.MapPath("~/UploadFile"), f.FileName);
+            if (!System.IO.File.Exists(path))
+            {
+                f.SaveAs(path);
+            }
+
+            //khai báo thư viện system.net.mime
+            Attachment data = new Attachment(Server.MapPath("~/UploadFile"+ f.FileName), MediaTypeNames.Application.Octet);
+            message.Attachments.Add(data);
+            //gửi email
+            mail.Send(message);
+            return View("SendMail");
+        }
+    }
+}
---
+++ SachOnline/Controllers/NhaXuatBanController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SachOnline.Models;
+
+namespace SachOnline.Controllers
+{
+    public class NhaXuatBanController : Controller
+    {
+        dbSachOnlineDataContext data = new dbSachOnlineDataContext();
+        // GET: NhaXuatBan
+        public ActionResult Index()
+        {
+            return View(from nxb in data.NHAXUATBANs select nxb);
+        }
+
+        //Cach 2
+        public ActionResult Details()
+        {
+            int manxb = int.Parse(Request.QueryString["id"]);
+            //int manxb = int.Parse(Request["id"]);
+            var result = data.NHAXUATBANs.Where(nxb => nxb.MaNXB == manxb).SingleOrDefault();
+            return View(result);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            NHAXUATBAN nxb = data.NHAXUATBANs.Where(n => n.MaNXB == id).SingleOrDefault();
+            return View(nxb);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Edit(NHAXUATBAN nxb)
+        {
+            if (ModelState.IsValid)
+            {
+                nxb.TenNXB = Request.Form["TenNXB"];
+                nxb.DiaChi = Request.Form["DiaChi"];
+                nxb.DienThoai= Request.Form["DienThoai"];
+                UpdateModel(nxb);
+                data.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Edit");
+            }
+        }
+    }
+}
